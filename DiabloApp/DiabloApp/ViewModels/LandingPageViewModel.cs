05fd6003a7f70f301c2b04a6d536c50607641337
Christian Wong <cwong@memoro.healthcare>
2023-06-07 12:40:19 -0400

    Extract viewmodel.

---
+++ DiabloApp/DiabloApp/ViewModels/LandingPageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DiabloApp.ViewModels
+{
+    public class LandingPageViewModel : BindableObject
+    {
+        private int _count = 1;
+        private string _counterText = "Click me to count up from 1!";
+        public string CounterText
+        {
+            get { return _counterText; }
+            set
+            {
+                if (_counterText != value)
+                {
+                    System.Console.WriteLine("\n\nProperty changed\n\n");
+                    _counterText = value;
+                    OnPropertyChanged(nameof(CounterText));
+                }
+            }
+        }
+        public ICommand IncrementCommand { get; }   // you can't set default values for non-static fields; they must be set in the ctor
+
+        public LandingPageViewModel()
+        {
+            this.IncrementCommand = new Command<string>(this.IncrementCounter);
+        }
+
+        private void IncrementCounter(string s)
+        {
+            int n = int.Parse(s);
+            this._count += n;
+            this.CounterText = $"Count: {this._count}";
+        }
+    }
+}
--- DiabloApp/DiabloApp/Views/LandingPage.xaml.cs
+++ DiabloApp/DiabloApp/Views/LandingPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Windows.Input;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace DiabloApp.Views
@@ -7,38 +6,10 @@ namespace DiabloApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LandingPage : ContentPage
     {
-        private int _count = 1;
-        private string _counterText = "Click me to count up from 2!";
-        public string CounterText
-        {
-            get { return _counterText; }
-            set
-            {
-                if (_counterText != value)
-                {
-                    System.Console.WriteLine("\n\nProperty changed\n\n");
-                    _counterText = value;
-                    OnPropertyChanged(nameof(CounterText));
-                }
-            }
-        }
-        public ICommand IncrementCommand { get; }   // you can't set default values for non-static fields; they must be set in the ctor
-
         public LandingPage()
         {
             InitializeComponent();
             this.ImageLanding.Source = ImageSource.FromResource("DiabloApp.Assets.LandingBackground.jpg");
-            this.IncrementCommand = new Command<string>(this.IncrementCounter);
-            this.BindingContext = this;
         }
-
-        private void IncrementCounter(string s)
-        {
-            System.Console.WriteLine($"\nIncrementing by {s}, which is a: {s.GetType().FullName}\n");
-            int n = int.Parse(s);
-            this._count += n;
-            this.CounterText = $"Count: {this._count}";
-        }
-
     }
 }
