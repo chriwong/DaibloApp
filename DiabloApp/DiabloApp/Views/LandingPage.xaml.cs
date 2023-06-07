a9e995ddd7741c1449da1afee311f81657565d80
Christian Wong <cwong@memoro.healthcare>
2023-06-07 12:25:19 -0400

    Added counting buttons.

--- DiabloApp/DiabloApp/Views/LandingPage.xaml.cs
+++ DiabloApp/DiabloApp/Views/LandingPage.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,15 +7,49 @@ namespace DiabloApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LandingPage : ContentPage
     {
+        private int _count = 1;
+        private string _counterText = "Click me to count up from 2!";
+        public string CounterText
+        {
+            get { return _counterText; }
+            set
+            {
+                if (_counterText != value)
+                {
+                    System.Console.WriteLine("\n\nProperty changed\n\n");
+                    _counterText = value;
+                    OnPropertyChanged(nameof(CounterText));
+                }
+            }
+        }
+        //public ICommand IncrementCommand { get; }   // you can't set default values for non-static fields; they must be set in the ctor
+
         public LandingPage()
         {
             InitializeComponent();
-            this.LandingImage.Source = ImageSource.FromResource("DiabloApp.Assets.LandingBackground.jpg");
+            this.ImageLanding.Source = ImageSource.FromResource("DiabloApp.Assets.LandingBackground.jpg");
+            //this.IncrementCommand = new Command<int>(this.IncrementCounter);
+            this.BindingContext = this;
         }
 
-        private void ProceedButton_Clicked(object sender, EventArgs e)
+        //private void IncrementCounter(int n)
+        //{
+        //    this._count += n;
+        //    this._count++;
+        //    this.CounterText = $"Count: {this._count}";
+        //}
+
+        private void ButtonOne_Clicked(object sender, System.EventArgs e)
         {
-            this.ProceedButton.Text= "TODO";
+            System.Console.WriteLine("\n\nButton 1 clicked\n\n");
+            this._count++;
+            this.CounterText = $"Count: {this._count}";
+        }
+        private void ButtonTwo_Clicked(object sender, System.EventArgs e)
+        {
+            System.Console.WriteLine("\n\nButton 2 clicked\n\n");
+            this._count += 2;
+            this.CounterText = $"Count: {this._count}";
         }
     }
 }
